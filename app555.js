2c40b2cc47c802e949b91ae2d79babab595780b7
man7909 <manoharkhojkipur629@gmail.com>
2026-06-04 12:21:57 +0530

    Add files via upload

---
+++ app555.js
@@ -0,0 +1,4 @@
+const { addcourse, removecourse } = require("./Addremove");
+
+console.log(addcourse("MongoDB"));
+console.log(removecourse("React"));
---
+++ coursemag1.js
@@ -0,0 +1,15 @@
+let courses = ['HTML','CSS','Javascript','React','Node.js'];
+
+function addcourse(course) {
+    courses.push(course);
+    return courses;
+}
+
+function removecourse(course) {
+    const index = courses.indexOf(course);
+    if (index !== -1) {
+        courses.splice(index, 1);
+    }
+    return courses;
+}
+module.exports = {addcourse,removecourse};
